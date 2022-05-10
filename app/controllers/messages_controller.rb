b9033b0c8ecfa121d1af722ab3afe504bcce4453
Kazuya-Sakashita <kz0508@gmail.com>
2022-05-10 22:11:34 +0900

    first commit

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,37 @@
+class MessagesController < ApplicationController
+  before_action :set_message, only: %i[destroy edit update]
+
+  def index
+    @messages = Message.all
+  end
+
+  def new
+  end
+
+  def create
+    @message = Message.create!(message_params)
+  end
+
+  def destroy
+    @message.destroy!
+  end
+
+   # 更新用のフォームに置き換えることだけに使用する
+  def edit
+  end
+
+  def update
+    @message.update!(message_params)
+  end
+
+  private
+
+  # Strong Parameters はサボらずに使っておくこととします
+  def message_params
+    params.require(:message).permit(:content)
+  end
+
+  def set_message
+    @message = Message.find(params[:id])
+  end
+end
